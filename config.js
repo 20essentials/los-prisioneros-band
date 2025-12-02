d5009597a3e2a63da9b8afd4f67d92fece6239e1
DominicDev <s0l0estudi4r@gmail.com>
2025-12-02 12:29:38 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LOS PRISIONEROS';
+export let nameSongs = [
+	"TREN AL SUR", /*1*/
+	"ESTRECHEZ DE CORAZÓN", /*2*/
+	"AMIGA MÍA", /*3*/
+	"CON SUAVIDAD", /*4*/
+	"CUENTAME UNA HISTORIA ORIGINAL", /*5*/
+	"SEXO", /*6*/
+	"LA VOZ DE LOS '80", /*7*/
+	"PARAMAR", /*8*/
+	"POR QUÉ NO SE VAN", /*9*/
+	"MUEVAN LAS INDUSTRIAS", /*10*/
+	"QUIEREN DINERO", /*11*/
+	"EL BAILE DE LOS QUE SOBRAN", /*12*/
+	"POR QUÉ LOS RICOS", /*13*/
+	"INDEPENDENCIA CULTURAL", /*14*/
+	"ESTAR SOLO", /*15*/
+	"POR FAVOR", /*16*/
+	"CUANDO TE VAYAS", /*17*/
+	"POR AMARTE", /*18*/
+	"PA PA PA", /*19*/
+	"¿QUIÉN MATÓ A MARILYN?", /*20*/
+]
